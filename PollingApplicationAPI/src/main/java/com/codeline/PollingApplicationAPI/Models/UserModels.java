f453571ea73278b99c8ca964b9fb629b0b5a8f3e
ahmedtechm <ahmedtechm2050@gmail.com>
2023-05-21 14:57:10 +0400

    Added id, username, and password fields to UserModels class

--- PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Models/UserModels.java
+++ PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Models/UserModels.java
@@ -1,5 +1,20 @@
 package com.codeline.PollingApplicationAPI.Models;
 
-public class UserModels {
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@Data
+@Setter
+@Getter
+public class UserModels {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+     Long id;
+
+     String username;
+     String password;
 }
