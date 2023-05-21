cdefb38480876821fce87741df813652f05f6658
ahmedtechm <ahmedtechm2050@gmail.com>
2023-05-21 16:36:39 +0400

    Added PollRepository dependency

--- PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Service/PollService.java
+++ PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Service/PollService.java
@@ -1,7 +1,12 @@
 package com.codeline.PollingApplicationAPI.Service;
 
+import com.codeline.PollingApplicationAPI.Repository.PollRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PollService {
+
+
+    PollRepository pollRepository;
 }
