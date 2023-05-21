08dc76195f4c9ade8feb75916f0b4cc8b6025eb4
ahmedtechm <ahmedtechm2050@gmail.com>
2023-05-21 14:25:47 +0400

    Added properties for tracking dates and status

--- PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Models/BaseEntity.java
+++ PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Models/BaseEntity.java
@@ -1,4 +1,18 @@
 package com.codeline.PollingApplicationAPI.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+
+@MappedSuperclass
+@Getter
+@Setter
+@Data
 public class BaseEntity {
+    Date createdDate;
+    Date updatedDate;
+    Boolean isActive;
 }
