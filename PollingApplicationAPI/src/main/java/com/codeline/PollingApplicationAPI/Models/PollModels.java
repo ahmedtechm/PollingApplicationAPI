605ce4b79f9f5a447981d2a658d20b2a1942f4f7
ahmedtechm <ahmedtechm2050@gmail.com>
2023-05-21 14:51:50 +0400

    Added id and question fields to Poll class

--- PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Models/PollModels.java
+++ PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Models/PollModels.java
@@ -1,4 +1,22 @@
 package com.codeline.PollingApplicationAPI.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Data
+@Entity
 public class PollModels {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String question;
 }
