d65f3b8333c518f9bc82ad9c575723bffe10b9ac
ahmedtechm <ahmedtechm2050@gmail.com>
2023-05-21 10:45:40 +0400

    Added Polling Application API Project to gitHub

---
+++ PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/PollingApplicationApiApplication.java
@@ -0,0 +1,13 @@
+package com.codeline.PollingApplicationAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingApplicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingApplicationApiApplication.class, args);
+	}
+
+}
---
+++ PollingApplicationAPI/src/test/java/com/codeline/PollingApplicationAPI/PollingApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeline.PollingApplicationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingApplicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
