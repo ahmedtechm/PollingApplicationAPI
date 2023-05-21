689e3b47a6dc09a09f4ad5b082ed3c71d5bb7976
ahmedtechm <ahmedtechm2050@gmail.com>
2023-05-21 16:15:41 +0400

    Added @Repository annotation to PollRepository and UserRepository class

--- PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Repository/PollRepository.java
+++ PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Repository/PollRepository.java
@@ -2,6 +2,8 @@ package com.codeline.PollingApplicationAPI.Repository;
 
 import com.codeline.PollingApplicationAPI.Models.PollModels;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PollRepository extends JpaRepository<PollModels, Long> {
 }
--- PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Repository/UserRepository.java
+++ PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.codeline.PollingApplicationAPI.Repository;
 
 import com.codeline.PollingApplicationAPI.Models.UserModels;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository  extends JpaRepository<UserModels, Long> {
 }
