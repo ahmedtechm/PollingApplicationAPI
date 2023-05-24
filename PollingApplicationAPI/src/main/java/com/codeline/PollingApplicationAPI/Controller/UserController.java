c7a27f85510a61734847e6294f5cba1d7d2275cc
ahmedtechm <ahmedtechm2050@gmail.com>
2023-05-24 11:22:19 +0400

    Added  @Autowired annotation to wire the UserService dependency

--- PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Controller/UserController.java
+++ PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Controller/UserController.java
@@ -1,7 +1,12 @@
 package com.codeline.PollingApplicationAPI.Controller;
 
+import com.codeline.PollingApplicationAPI.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    UserService userService;
 }
