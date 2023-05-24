39a49863185a63ec446fec6b18fe2c74e717ff3d
ahmedtechm <ahmedtechm2050@gmail.com>
2023-05-24 11:50:19 +0400

    Added createUser() method to create a new user

--- PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Controller/UserController.java
+++ PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Controller/UserController.java
@@ -1,5 +1,6 @@
 package com.codeline.PollingApplicationAPI.Controller;
 
+import com.codeline.PollingApplicationAPI.Models.User;
 import com.codeline.PollingApplicationAPI.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,5 +15,12 @@ public class UserController {
     @RequestMapping("user/create")
     public void saveUser () {
         createUser();
+    }
+        public void createUser() {
+        User user = new User();
+        user.setUsername("ahmed");
+        user.setPassword("12221");
+        userService.saveUser(user);
+
     }
 }
