9fd7f835a2e7cda8d66fa50e74aee3f76cb580e4
ahmedtechm <ahmedtechm2050@gmail.com>
2023-05-24 15:44:18 +0400

    Added @Getter, @Setter, @AllArgsConstructor, and @NoArgsConstructor annotations to GetUserRequest

--- PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/RequestObjects/GetUserRequest.java
+++ PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/RequestObjects/GetUserRequest.java
@@ -1,6 +1,14 @@
 package com.codeline.PollingApplicationAPI.RequestObjects;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetUserRequest {
 
     String username;
