89256e2b1326a95fade04cc1b5793b894a69b289
ahmedtechm <ahmedtechm2050@gmail.com>
2023-05-25 09:09:51 +0400

    Implement createPoll method to save a new poll

--- PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Controller/PollController.java
+++ PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Controller/PollController.java
@@ -1,9 +1,12 @@
 package com.codeline.PollingApplicationAPI.Controller;
 
+import com.codeline.PollingApplicationAPI.Models.Poll;
 import com.codeline.PollingApplicationAPI.Service.PollService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 public class PollController {
@@ -13,5 +16,14 @@ public class PollController {
     @RequestMapping("poll/create")
     public void savePoll () {
         createPoll();
+    }
+
+    public void createPoll() {
+
+        Poll poll = new Poll();
+        poll.setQuestion("");
+        poll.setCreatedDate(new Date());
+        poll.setIsActive(true);
+        pollService.savePoll(poll);
     }
 }
