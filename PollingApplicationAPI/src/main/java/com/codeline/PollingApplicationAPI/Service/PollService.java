a9e1748e6e28ae9732936533a8ce096d786e592f
ahmedtechm <ahmedtechm2050@gmail.com>
2023-05-25 08:56:04 +0400

    Added method to save a Poll object

--- PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Service/PollService.java
+++ PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Service/PollService.java
@@ -1,5 +1,6 @@
 package com.codeline.PollingApplicationAPI.Service;
 
+import com.codeline.PollingApplicationAPI.Models.Poll;
 import com.codeline.PollingApplicationAPI.Repository.PollRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,9 @@ public class PollService {
 
     @Autowired
     PollRepository pollRepository;
+
+    public void savePoll(Poll poll) {
+        pollRepository.save(poll);
+    }
 }
+
