597e68dd1fe885e35a99fc182505851c7e513a55
ahmedtechm <ahmedtechm2050@gmail.com>
2023-05-25 09:01:42 +0400

    Added savePoll method and request mapping for creating a poll

--- PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Controller/PollController.java
+++ PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Controller/PollController.java
@@ -2,10 +2,16 @@ package com.codeline.PollingApplicationAPI.Controller;
 
 import com.codeline.PollingApplicationAPI.Service.PollService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PollController {
     @Autowired
     PollService pollService;
+
+    @RequestMapping("poll/create")
+    public void savePoll () {
+        createPoll();
+    }
 }
