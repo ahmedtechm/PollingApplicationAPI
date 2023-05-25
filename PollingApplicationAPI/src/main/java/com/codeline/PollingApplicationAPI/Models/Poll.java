cfcb863c6c31561382d7392b23388dc0f556a082
ahmedtechm <ahmedtechm2050@gmail.com>
2023-05-25 13:42:12 +0400

    Initialize choices as an ArrayList of Strings

--- PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Models/Poll.java
+++ PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Models/Poll.java
@@ -4,10 +4,8 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -22,7 +20,7 @@ public class Poll  extends BaseEntity {
     Long id;
 
     String question;
-    List<String> choices;
-    Map<String, Integer> voteCounts;
+
+    List<String> choices = new ArrayList<>();
 
 }
