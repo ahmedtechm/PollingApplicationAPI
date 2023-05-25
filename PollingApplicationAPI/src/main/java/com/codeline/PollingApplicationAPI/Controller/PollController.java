395696890eab1064eb5e872f6c7a2f712bc471ec
ahmedtechm <ahmedtechm2050@gmail.com>
2023-05-25 08:51:41 +0400

    Added @Autowired annotation for PollService

--- PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Controller/PollController.java
+++ PollingApplicationAPI/src/main/java/com/codeline/PollingApplicationAPI/Controller/PollController.java
@@ -1,7 +1,11 @@
 package com.codeline.PollingApplicationAPI.Controller;
 
+import com.codeline.PollingApplicationAPI.Service.PollService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PollController {
+    @Autowired
+    PollService pollService;
 }
